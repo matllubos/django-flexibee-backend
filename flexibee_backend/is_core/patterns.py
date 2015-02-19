369adcf46705f59508dfa56bd1e73b30d007c132
Lubos Matl <matllubos@gmail.com>
2015-02-19 11:47:04 +0100

    0.2.12 Changed patterns according to new is-core

--- flexibee_backend/is_core/patterns.py
+++ flexibee_backend/is_core/patterns.py
@@ -5,18 +5,11 @@ from is_core.patterns import RestPattern, UIPattern
 
 class FlexibeePattern(object):
 
-    def get_kwargs(self, request):
-        kwargs = {}
+    def _get_try_kwargs(self, request, obj):
+        kwargs = super(FlexibeePattern, self)._get_try_kwargs(request, obj)
         if '(?P<company_pk>[-\w]+)' in self.url_prefix or '(?P<company_pk>[-\w]+)' in self.url_pattern:
             kwargs['company_pk'] = request.kwargs.get('company_pk')
         return kwargs
-
-    def get_url_string(self, request, obj=None, kwargs=None):
-        kwargs = (kwargs or {}).copy()
-        kwargs.update(self.get_kwargs(request))
-        if obj:
-            kwargs.update(self._get_try_kwarg(obj))
-        return reverse(self.pattern, kwargs=kwargs)
 
 
 class FlexibeeRestPattern(FlexibeePattern, RestPattern):
